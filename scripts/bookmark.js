c05baca4a519cdf7f097b7fb69cf4adf4c38f51d
Xaru <Xaru@Live.de>
2020-09-22 22:11:28 +0200

    Update bookmark.js
    Changed outdated domain.

--- scripts/bookmark.js
+++ scripts/bookmark.js
@@ -1,5 +1,5 @@
 javascript:(function() {
-	const CORE  = "https://rawgit.com/Xaruh/loader/master/scripts/core.js";
+	const CORE  = "https://raw.githubusercontent.com/Xaruh/Loader/master/scripts/core.js";
 	var Request = new XMLHttpRequest();
 	Request.open("GET", CORE);
 	Request.send();
